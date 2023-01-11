4b273eaeb457db5e9a7c26e30b6b0ad0178e4013
lou <loujr@github.com>
2023-01-11 03:05:25 +0000

    stonk feature

--- miketyson.py
+++ miketyson.py
@@ -4,11 +4,14 @@ import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-import re # regex
+import re  # regex
+import requests
+import json
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD = os.getenv("DISCORD_GUILD")
+RAPIDAPI_TOKEN = os.getenv("RAPIDAPI_TOKEN")
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=".", intents=intents)
@@ -42,16 +45,47 @@ async def on_message(message):
     # responds w/ thats my boy right there if lou is found in message
 
 
+@bot.command(pass_context=True)
+async def stonk(ctx, arg):
+    symbol = arg
+    url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-quotes"
+    querystring = {"region": "US", "symbols": symbol}
+    headers = {
+        "x-rapidapi-key": RAPIDAPI_TOKEN,
+        "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+    }
+    response = requests.get(url,headers=headers,params=querystring)
+    data = response.json()
+    result = data["quoteResponse"]["result"][0]
+    name = result["longName"]
+    ticker = result["symbol"]
+    current_market_price = result["regularMarketPrice"]
+    market_high = result["regularMarketDayHigh"]
+    market_low = result["regularMarketDayLow"]
+    market_cap = result["marketCap"]
+    volume = result["regularMarketVolume"]
+
+    embed = discord.Embed(title=name, color=0x15DBC7)
+    embed.add_field(name="Ticker", value=ticker, inline=False)
+    embed.add_field(name="High", value=market_high, inline=False)
+    embed.add_field(name="Low", value=market_low, inline=False)
+    await ctx.send(embed=embed)
+
+    # .stonk <ticker>   returns stonk chart
+
+
 @bot.command()
 async def tableflip(ctx):
     await ctx.send("(╯°□°)╯︵ ┻━┻")
 
     # .tableflip
 
+
 @bot.command()
 async def ping(ctx):
     await ctx.send("pong")
 
     # .pong
+
 
 bot.run(TOKEN)
